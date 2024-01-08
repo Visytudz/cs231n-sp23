b19005ed6d9f3a49cda69e99c2a9bdca2d397ea3
Visytudz <guowei_zhang@zju.edu.cn>
2024-01-08 16:11:19 +0800

    finish softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -34,9 +34,26 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    train_num, dim = X.shape
+    class_num = W.shape[1]
 
-    pass
+    for i in range(train_num):
+        score = X[i].dot(W)
+        score = score - np.max(score)
+        e_score = np.exp(score)
+        prob = e_score / np.sum(e_score)
+        loss += -np.log(prob[y[i]])
 
+        for j in range(class_num):
+            if j == y[i]:
+                dW[:, j] += X[i] * (-1 + prob[j])
+            else:
+                dW[:, j] += X[i] * prob[j]
+
+    loss /= train_num
+    loss += reg * np.sum(W * W)
+    dW /= train_num
+    dW += 2 * reg * W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -59,8 +76,23 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    train_num, dim = X.shape
+    class_num = W.shape[1]
+    coeff = np.zeros((train_num, class_num))
 
-    pass
+    scores = X.dot(W)
+    scores = scores - np.max(scores, axis=1)[:, np.newaxis]
+    e_scores = np.exp(scores)
+    prob = e_scores / np.sum(e_scores, axis=1)[:, np.newaxis]
+    correct_prob = prob[np.arange(train_num), y]
+    loss = -np.log(correct_prob)
+    loss = np.mean(loss) + reg * np.sum(W * W)
+
+    coeff = prob
+    coeff[np.arange(train_num), y] -= 1
+
+    dW = X.T.dot(coeff)
+    dW = dW / train_num + 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
